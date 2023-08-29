ec8a8975d752a508d86aa68a9a196793cad34a4c
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-08-29 17:36:49 -0400

    Correct frontend build error

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -32,7 +32,7 @@ export default function Login() {
     // We don't want the page to refresh
     e.preventDefault()
     setIsLoading(true) // Set loading to true when the request starts
-      
+
     try {
       const formData = new FormData(e.currentTarget)
       const response = await fetch('/api/submit', {
@@ -42,18 +42,18 @@ export default function Login() {
           'accept': 'application/json',
         },
       })
-      .then((response) => response.json())
-      .then((data) => {
-        setFormData({ 
-          firstName: "",
-          lastName: "",
-          username: "",
-          email: "",
-          password: "",
-          accountType: [], 
+        .then((response) => response.json())
+        .then((data) => {
+          setFormData({
+            firstName: "",
+            lastName: "",
+            username: "",
+            email: "",
+            password: "",
+            accountType: [],
+          })
+          setFormSuccessMessage(data.submission_text)
         })
-        setFormSuccessMessage(data.submission_text)
-      })
     } catch (error) {
       console.error(error)
     } finally {
@@ -66,71 +66,47 @@ export default function Login() {
     <>
       <Navbar />
       <div>
-        {formSuccess ? 
-          <div>{formSuccessMessage}</div> 
-          : 
-          // <form method="POST" action="https://www.formbackend.com/f/664decaabbf1c319" onSubmit={submitForm}>
-          //   <div>
-          //     <label>First Name</label>
-          //     <input type="text" name="firstName" onChange={handleInput} value={formData.firstName} />
-          //   </div>
-          //   <div>
-          //     <label>Last Name</label>
-          //     <input type="text" name="lastName" onChange={handleInput} value={formData.lastName} />
-          //   </div>
-          //   <div>
-          //     <label>Username</label>
-          //     <input type="text" name="username" onChange={handleInput} value={formData.username} />
-          //   </div>
-          //   <div>
-          //     <label>Password</label>
-          //     <input type="text" name="password" onChange={handleInput} value={formData.password} />
-          //   </div>
-          //   <div>
-          //     <label>Email</label>
-          //     <input type="text" name="email" onChange={handleInput} value={formData.email} />
-          //   </div>
-
-          //   <button type="submit">Send message</button>
-          // </form>
+        {formSuccess ?
+          <div>{formSuccessMessage}</div>
+          :
           <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-          </div>
-        
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username or Email</label>
-                <div className="mt-2">
-                  <input onChange={handleInput} value={formData.username} id="username" name="username" type="username" autoComplete="username" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                </div>
-              </div>
-        
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username or Email</label>
+                  <div className="mt-2">
+                    <input onChange={handleInput} value={formData.username} id="username" name="username" type="username" autoComplete="username" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   </div>
                 </div>
-                <div className="mt-2">
-                  <input onChange={handleInput} value={formData.password} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input onChange={handleInput} value={formData.password} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
                 </div>
-              </div>
-        
-              <div>
-                <button type="submit" disabled={isLoading} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{isLoading ? 'Loading...' : 'Sign In'}</button>
-              </div>
-            </form>
-        
-            <p className="mt-10 text-center text-sm text-gray-500">
-              <span>Don't have an account? </span>
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create an account</a>
-            </p>
+
+                <div>
+                  <button type="submit" disabled={isLoading} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{isLoading ? 'Loading...' : 'Sign In'}</button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                <span>No account yet? </span>
+                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create an account</a>
+              </p>
+            </div>
           </div>
-        </div>
-          
+
 
         }
       </div>
