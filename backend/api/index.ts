748f21b8dd8cc910719d1bfa3e764f26ef71178e
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-08-29 18:33:36 -0400

    Try for vercel api 7

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,62 +1,16 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-const userRouter = require("../src/routes/userRouter");
+const app = require('express')();
+const { v4 } = require('uuid');
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.send('Hello World again!');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-
-app.get('/404', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.send('OOPs!');
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// Routes
-app.use("/api/users", userRouter);
-
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('../routes/index');
-
-// var app = express();
-
-// const whitelist = [
-//   '*'
-// ];
-
-// app.use((req, res, next) => {
-//   const origin = req.get('referer');
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === 'OPTIONS') res.sendStatus(200);
-//   else next();
-// });
-
-// const setContext = (req, res, next) => {
-//   if (!req.context) req.context = {};
-//   next();
-// };
-// app.use(setContext);
-
-// app.use('/', indexRouter);
 
 module.exports = app;
