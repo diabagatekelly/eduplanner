14a67829994b2d183aaa086a2a4e3bfba385d752
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-08-29 19:00:07 -0400

    remove vercel from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ const port = 8000;
 import ConnectMongodb from './mongodb/connectMongodb';
 import SeedDefaultDb from './mongodb/seedDefaultDb';
 
+const userRouter = require("./routes/userRouter");
+
 app.get('/', (req, res) => {
   res.send('Hello World again!');
 });
@@ -16,8 +18,8 @@ app.listen(port, () => {
 new ConnectMongodb().run().catch(console.error)
 new SeedDefaultDb().run().catch(console.error)
 
-
-
+// Routes
+app.use("/api/users", userRouter);
 
 
 
