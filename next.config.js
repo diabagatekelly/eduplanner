1b2dfe7d260aaa3c2518e938531875f6b32470c7
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-03 18:42:07 -0400

    ci/cd update + env 3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXT_BASE_URL: "https://eduplanner-backend-7fdf262835f2.herokuapp.com"
+    },
+}
 
 module.exports = nextConfig
