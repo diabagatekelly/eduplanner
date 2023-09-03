9f52f1a6872a42e37a2e72d47a266eba38e2fe8a
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-03 17:37:37 -0400

    ci/cd update

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,16 +62,16 @@ const config: PlaywrightTestConfig = {
     //   },
     // },
     // Test against mobile viewports.
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'Mobile Safari',
-      use: devices['iPhone 12'],
-    },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: devices['iPhone 12'],
+    // },
   ],
 }
 export default config
