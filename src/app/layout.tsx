ed1ebd1737d6217831c187e1abc8cb15114627a4
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-08 18:21:41 -0400

    Add provider and store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Provider } from "react-redux"
+import store from "./store"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider store={store}>
+          {children}
+        </Provider>
+      </body>
     </html>
   )
 }
