1757a9ea8dd3c7a006cbe69158ad09548f78580d
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-08 19:13:27 -0400

    Mock reducer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+"use client";
 import Navbar from "./ui/navbar"
+import { decrement, increment, reset } from "@/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export default function Home() {
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <Navbar />
-      <h1>Hello, Next.js!</h1>
+      <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+        <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+          <h4 style={{ marginBottom: 16 }}>{count}</h4>
+          <button onClick={() => dispatch(increment())}>increment</button>
+          <button
+            onClick={() => dispatch(decrement())}
+            style={{ marginInline: 16 }}
+          >
+            decrement
+          </button>
+          <button onClick={() => dispatch(reset())}>reset</button>
+        </div>
+      </main>
     </>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 // import authReducer from "./reducers/authReducer";
+import counterReducer from "./features/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    // authReducer
+    counterReducer,
   }
 })
 
