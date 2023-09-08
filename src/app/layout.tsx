69e87c5b0ff34d983ec8751b9b136b47c9f8165d
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-08 18:35:35 -0400

    Reconnect redux store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Provider } from "react-redux"
-import store from "./store"
+import { Providers } from "@/redux/provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,11 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Provider store={store}>
-          {children}
-        </Provider>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
