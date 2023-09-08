7baa034b8c412180530a3df70210fa9b0c4232ca
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-08 11:19:17 -0400

    Add CI to env variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_BASE_URL: "https://eduplanner-backend-7fdf262835f2.herokuapp.com"
+        NEXT_BASE_URL: "https://eduplanner-backend-7fdf262835f2.herokuapp.com",
+        CI: false
     },
 }
 
