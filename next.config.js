857001151a0ed6dddfb0ba84e75ad13195253e96
Kelly Diabagate <diabagatekelly@yahoo.fr>
2023-09-16 13:48:25 -0400

    Combine next config js with process.env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_BASE_URL: process.env.NEXT_BASE_URL,
+        CI: false
+    },
 }
 
 module.exports = nextConfig
