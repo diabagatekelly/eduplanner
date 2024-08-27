ed01bf82d186e1650364bcb3cd952745f4f5c345
Kelly Diabagate <diabagatekelly@yahoo.fr>
2024-08-26 21:25:11 -0400

    Fix nested layout bug where we were looking for main user activity while on student page

--- src/app/nested-layout.tsx
+++ src/app/nested-layout.tsx
@@ -13,6 +13,7 @@ function classNames(...classes) {
 export default function NestedLayout({ children, isTeacher }: {children: any, isTeacher: boolean}) {
   const pathname = usePathname();
   const activityPath = useParams().activity;
+  const student = useParams().student as string;
   let args;
 
   const [user, getUserData] = useState<IUser>({ ...args })
@@ -20,10 +21,18 @@ export default function NestedLayout({ children, isTeacher }: {children: any, is
   const [openDrawer, setOpenDrawer] = useState(true)
   
   useEffect(() => {
-    const { userReducer } = store.getState()
+    const { userReducer }: {userReducer: IUser} = store.getState()
     getUserData(userReducer);
 
-    const cardSubMenu = userReducer?.activities?.find(activity => activity.name === activityPath)?.hasCards
+    const isMain = !student;
+    let cardSubMenu;
+
+    if (isMain) {
+      cardSubMenu = userReducer?.activities?.find(activity => activity.name === activityPath)?.hasCards
+    } else {
+      cardSubMenu = userReducer?.students?.[student]?.activities?.find(activity => activity.name === activityPath)?.hasCards      
+    }
+
     showCardSubMenu(cardSubMenu)
   }, [activityPath])
 
@@ -72,17 +81,17 @@ export default function NestedLayout({ children, isTeacher }: {children: any, is
                     <span className="flex-1 ml-3 whitespace-nowrap">Activity: {fromDbFormat(activityPath)}</span>
                   </a>
                 </li>
-                <li>
+                <li className='menu-item-cards'>
                   <a href={`${pathname}/#active`} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <span className="flex-1 ml-7 whitespace-nowrap italic">View All Active Cards</span>
                   </a>
                 </li>
-                <li>
+                <li className='menu-item-cards'>
                   <a href={`${pathname}/#inactive`} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <span className="flex-1 ml-7 whitespace-nowrap italic">View All Inactive Cards</span>
                   </a>
                 </li>
-                <li>
+                <li className='menu-item-cards'>
                   <a href={`${pathname}/#add`} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <span className="flex-1 ml-7 whitespace-nowrap italic">Add new card</span>
                   </a>
--- src/specs/app/nested-layout.test.tsx
+++ src/specs/app/nested-layout.test.tsx
@@ -4,7 +4,7 @@ import { act, fireEvent, screen } from '@testing-library/react';
 import { render } from '../util';
 import * as React from 'react';
 import Dashboard from '../../components/dashboard';
-import { mockUser, mockActivity } from '../mocks';
+import { mockUser, mockActivity, mockStudent, mockLanguageActivity } from '../mocks';
 import store from '../../store/store';
 
 jest.mock('next/navigation', () => {
@@ -71,8 +71,37 @@ describe('Nested layout', () => {
     render(<NestedLayout {...{children: <Dashboard {...{userDetails, isMain: true, isTeacher: false}}/>, isTeacher: false}}/>)
 
     const menuItemDashboard = document.querySelectorAll('.menu-item')[0].querySelector('a') as Element;
+    const menuItemActivity = document.querySelectorAll('.menu-item');
+
     const highlightedClass = "bg-gray-800 text-white hover:bg-gray-700"
   
     expect(menuItemDashboard).toHaveClass(highlightedClass)
+    expect(menuItemActivity[2]).toBeUndefined()
+  })
+
+  it('should display cards submenu for main user', async () => {
+    jest.spyOn(require('next/navigation'), 'usePathname').mockImplementation(() => '/mock-user')
+    jest.spyOn(require('next/navigation'), 'useParams').mockImplementation(() => { return {activity: 'Quran', student: undefined}})
+    const userDetails = {...mockUser, activities: [{...mockActivity, hasCards: true}]}
+    const mockStoreState = {authReducer: {isAuthenticated: true}, userReducer: userDetails}
+    jest.spyOn(store, 'getState').mockReturnValue(mockStoreState);
+    render(<NestedLayout {...{children: <Dashboard {...{userDetails, isMain: true, isTeacher: false}}/>, isTeacher: false}}/>)
+
+    const cardSubmenu = document.querySelectorAll('.menu-item-cards')[0] as Element;
+    expect(cardSubmenu).toHaveTextContent('Cards')
+
+  })
+
+  it('should display cards submenu for main user', async () => {
+    jest.spyOn(require('next/navigation'), 'usePathname').mockImplementation(() => '/mock-user/students/mock-student/activities/Arabic-Language')
+    jest.spyOn(require('next/navigation'), 'useParams').mockImplementation(() => { return {activity: 'Arabic-Language', student: 'mock-student'}})
+    const userDetails = {...mockUser, students: {'mock-student': {...mockStudent, activities: [{...mockLanguageActivity, hasCards: true}]}}, activities: [{...mockActivity}]}
+    const mockStoreState = {authReducer: {isAuthenticated: true}, userReducer: userDetails}
+    jest.spyOn(store, 'getState').mockReturnValue(mockStoreState);
+    render(<NestedLayout {...{children: <Dashboard {...{userDetails, isMain: false, isTeacher: false}}/>, isTeacher: false}}/>)
+
+    const cardSubmenu = document.querySelectorAll('.menu-item-cards')[0] as Element;
+    expect(cardSubmenu).toHaveTextContent('Cards')
+
   })
 })
